443e3ee6c538cfd3df3c5be384946582209989e3
Pete Mullins <jc310xc@gmail.com>
2022-05-07 18:03:05 -0400

    AppAuth wip

--- app/src/main/java/com/mullipr/festie/MainActivity.kt
+++ app/src/main/java/com/mullipr/festie/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.mullipr.festie
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.openid.appauth.AuthorizationRequest
+import net.openid.appauth.AuthorizationServiceConfiguration
+import net.openid.appauth.ResponseTypeValues
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val serviceConfig = AuthorizationServiceConfiguration(
+            Uri.parse("https://idp.example.com/auth"),  // authorization endpoint
+            Uri.parse("https://idp.example.com/token")
+        )
+
+        val authRequestBuilder: AuthorizationRequest = AuthorizationRequest.Builder(
+            serviceConfig,
+            "9d2216a850c5414f9986f99f6e2f2907",
+            ResponseTypeValues.CODE,
+            Uri.parse("com.mullipr.festie:/authorized")
+        ).setScope("playlist-read-collaborative playlist-modify-public user-modify-playback-state user-read-playback-state user-read-currently-playing app-remote-control")
+            .build()
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
