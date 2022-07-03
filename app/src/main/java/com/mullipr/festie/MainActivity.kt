8cc5a06c0ad940372376384d1852ca081a0d70d2
Pete Mullins <jc310xc@gmail.com>
2022-07-03 12:14:11 -0400

    implement happy path for OAuth flow

--- app/src/main/java/com/mullipr/festie/MainActivity.kt
+++ app/src/main/java/com/mullipr/festie/MainActivity.kt
@@ -3,23 +3,29 @@ package com.mullipr.festie
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import net.openid.appauth.AuthorizationRequest
-import net.openid.appauth.AuthorizationServiceConfiguration
-import net.openid.appauth.ResponseTypeValues
+import net.openid.appauth.*
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val RC_AUTH = 100;
+    }
+
+    lateinit var authService : AuthorizationService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val serviceConfig = AuthorizationServiceConfiguration(
-            Uri.parse("https://idp.example.com/auth"),  // authorization endpoint
-            Uri.parse("https://idp.example.com/token")
+            Uri.parse("https://accounts.spotify.com/authorize"),  // authorization endpoint
+            Uri.parse("https://accounts.spotify.com/api/token")
         )
 
-        val authRequestBuilder: AuthorizationRequest = AuthorizationRequest.Builder(
+        val authRequest: AuthorizationRequest = AuthorizationRequest.Builder(
             serviceConfig,
             "9d2216a850c5414f9986f99f6e2f2907",
             ResponseTypeValues.CODE,
@@ -27,10 +33,30 @@ class MainActivity : AppCompatActivity() {
         ).setScope("playlist-read-collaborative playlist-modify-public user-modify-playback-state user-read-playback-state user-read-currently-playing app-remote-control")
             .build()
 
+        authService = AuthorizationService(this)
+        val authIntent = authService.getAuthorizationRequestIntent(authRequest)
 
+        startActivityForResult(authIntent, RC_AUTH)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_AUTH && data != null) {
+            val authResponse = AuthorizationResponse.fromIntent(data)
+            val exception = AuthorizationException.fromIntent(data)
+
+            if(authResponse == null)
+                return
+
+            authService.performTokenRequest(authResponse.createTokenExchangeRequest()
+            ) { response, _ ->
+                if(response != null){
+                    val time = System.currentTimeMillis()
+                    Log.d("[festie]", "Current time: $time")
+                    Log.d("[festie]", response.accessTokenExpirationTime.toString())
+                    Toast.makeText(this, "WE HAVE A TOKEN: ${response.accessToken}", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
